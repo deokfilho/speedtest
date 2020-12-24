820410b384c599519c6e0df97ff7d30504bdb6fb
Deok Filho <deokcelmofilho@gmail.com>
2020-12-24 17:42:38 -0300

    changing original file path for better understanding

--- speedtest_script.sh
+++ speedtest_script.sh
@@ -1,22 +1,22 @@
-/Users/deok/Documents/Coding/speedtest/speedtest > /Users/deok/Documents/Coding/speedtest/temp_speedtest.txt &&
-date +%x | xargs echo -n >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-echo -n "," >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-date +%T | xargs echo -n >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-echo -n "," >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-date +%Z | xargs echo -n >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-echo -n "," >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-cat /Users/deok/Documents/Coding/speedtest/temp_speedtest.txt | grep "Server" | cut -d' ' -f7- | xargs echo -n >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-echo -n "," >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-cat /Users/deok/Documents/Coding/speedtest/temp_speedtest.txt | grep "ISP" | cut -d' ' -f7- | xargs echo -n >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-echo -n "," >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-cat /Users/deok/Documents/Coding/speedtest/temp_speedtest.txt | grep "Latency" | awk '{print $2}' | xargs echo -n >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-echo -n "," >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-cat /Users/deok/Documents/Coding/speedtest/temp_speedtest.txt | grep "Download" | awk '{print $3}' | xargs echo -n >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-echo -n "," >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-cat /Users/deok/Documents/Coding/speedtest/temp_speedtest.txt | grep "Download" | awk '{print $7}' | xargs echo -n >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-echo -n "," >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-cat /Users/deok/Documents/Coding/speedtest/temp_speedtest.txt | grep "Upload" | awk '{print $3}' | xargs echo -n >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-echo -n "," >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-cat /Users/deok/Documents/Coding/speedtest/temp_speedtest.txt | grep "Upload" | awk '{print $7}' | xargs echo -n >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-echo -n "," >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt &&
-cat /Users/deok/Documents/Coding/speedtest/temp_speedtest.txt | grep "Packet Loss" | cut -d' ' -f7- >> /Users/deok/Documents/Coding/speedtest/daily_speedtest.txt
+/my-path/speedtest > /my-path/temp_speedtest.txt &&
+date +%x | xargs echo -n >> /my-path/daily_speedtest.txt &&
+echo -n "," >> /my-path/daily_speedtest.txt &&
+date +%T | xargs echo -n >> /my-path/daily_speedtest.txt &&
+echo -n "," >> /my-path/daily_speedtest.txt &&
+date +%Z | xargs echo -n >> /my-path/daily_speedtest.txt &&
+echo -n "," >> /my-path/daily_speedtest.txt &&
+cat /my-path/temp_speedtest.txt | grep "Server" | cut -d' ' -f7- | xargs echo -n >> /my-path/daily_speedtest.txt &&
+echo -n "," >> /my-path/daily_speedtest.txt &&
+cat /my-path/temp_speedtest.txt | grep "ISP" | cut -d' ' -f7- | xargs echo -n >> /my-path/daily_speedtest.txt &&
+echo -n "," >> /my-path/daily_speedtest.txt &&
+cat /my-path/temp_speedtest.txt | grep "Latency" | awk '{print $2}' | xargs echo -n >> /my-path/daily_speedtest.txt &&
+echo -n "," >> /my-path/daily_speedtest.txt &&
+cat /my-path/temp_speedtest.txt | grep "Download" | awk '{print $3}' | xargs echo -n >> /my-path/daily_speedtest.txt &&
+echo -n "," >> /my-path/daily_speedtest.txt &&
+cat /my-path/temp_speedtest.txt | grep "Download" | awk '{print $7}' | xargs echo -n >> /my-path/daily_speedtest.txt &&
+echo -n "," >> /my-path/daily_speedtest.txt &&
+cat /my-path/temp_speedtest.txt | grep "Upload" | awk '{print $3}' | xargs echo -n >> /my-path/daily_speedtest.txt &&
+echo -n "," >> /my-path/daily_speedtest.txt &&
+cat /my-path/temp_speedtest.txt | grep "Upload" | awk '{print $7}' | xargs echo -n >> /my-path/daily_speedtest.txt &&
+echo -n "," >> /my-path/daily_speedtest.txt &&
+cat /my-path/temp_speedtest.txt | grep "Packet Loss" | cut -d' ' -f7- >> /my-path/daily_speedtest.txt
